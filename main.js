4266dc1d6b6092fe9992b52cea854830b1634527
Oscar Muñoz <oscar_m_f@hotmail.com>
2023-03-20 22:37:28 +0100

    OMF: example main

---
+++ main.js
@@ -0,0 +1,37 @@
+/*
+ function onLap() { trace('--- onLap ---'); }
+ function onAutoLap() { trace('--- onAutoLap ---'); }
+ function onInterval() { trace('--- onInterval ---'); }
+ function onPoolLength() { trace('--- onPoolLength ---'); }
+ function onExerciseStart() { trace('--- onExerciseStart ---'); }
+ function onExercisePause() { trace('--- onExercisePause ---'); }
+ function onExerciseContinue() { trace('--- onExerciseContinue ---'); }
+ function onExerciseEnd() { trace('--- onExerciseEnd ---'); }
+ function onExercisePause() { trace('--- onExerciseEnd ---'); }
+ function onExerciseContinue() { trace('--- onExerciseEnd ---'); }
+ */
+
+
+function evaluate(input, output) {  
+}
+ 
+ function getUserInterface(input, output) {
+   return {
+    template: 't'
+   };
+ }
+ 
+ // This is called also when user backs from exercise start panel without starting
+ // exercise. onExerciseEnd() is not working at all as zapp gets disabled before
+ // it is called (and it would be called only when exercise is really started).
+ function getSummaryOutputs(input, output) {
+   return [
+     {
+      // Save the data of number of times you make a route or ascent/descent in indoor climbing into SA.
+      id: 'climbAttempts',
+      name: "Nº of Attempts",
+      format: 'Count_Threedigits',
+      value: output.climbAttempts
+     },
+   ];
+ }
